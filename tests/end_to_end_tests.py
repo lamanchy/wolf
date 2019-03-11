7287b1ceeea888528ad504628cc8fe8bca4d71fb
Ondřej Lomič <ondrej.lomic@ysoft.com>
2019-03-11 15:26:34 +0100

    better test class

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -17,19 +17,25 @@ class BaseTest(object):
         assert rc == 0
         build_lock.release()
 
-
-class TestStringMethods(BaseTest, unittest.TestCase):
     def setUp(self):
-        self.assertTrue(isfile("../cmake-build-debug/parser"))
+        assert isfile("../cmake-build-debug/parser")
         self.wolf = Popen("../cmake-build-debug/parser", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-    def test_upper(self):
-        stdout, stderr = self.wolf.communicate(
-            """{"@timestamp": "2018-06-21T15:00:00.006Z", "message": "starting stuff 1", "group": "default", "host": "localhost"}""")
+    def tearDown(self):
+        pass
 
-        self.assertTrue(
-            """{"@timestamp":"2018-06-21T15:00:00.006Z","algorithm":"elapsed","elapsedId":"stuff_elapsed","group":"default","host":"localhost","position":"start","type":"correlation_data","uniqueId":"1"}"""
-            in stdout)
+    def for_input_expects_output(self, input, output):
+        stdout, stderr = self.wolf.communicate(input)
+        assert output in stdout
+        # assert len(stderr) == 0
+        assert self.wolf.returncode == 0
+
+
+class TestStringMethods(BaseTest, unittest.TestCase):
+    def test_upper(self):
+        input = """{"@timestamp": "2018-06-21T15:00:00.006Z", "message": "starting stuff 1", "group": "default", "host": "localhost"}"""
+        output = """{"@timestamp":"2018-06-21T15:00:00.006Z","algorithm":"elapsed","elapsedId":"stuff_elapsed","group":"default","host":"localhost","position":"start","type":"correlation_data","uniqueId":"1"}"""
+        self.for_input_expects_output(input, output)
 
 
 if __name__ == '__main__':
