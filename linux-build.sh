a9dd9b732d0d2f9c43d80271d6e9b2f492374f26
Ondřej Lomič <ondrej.lomic@ysoft.com>
2019-03-22 23:01:13 +0100

    better docker run

--- linux-build.sh
+++ linux-build.sh
@@ -6,7 +6,7 @@ if [[ "$(sudo docker images -q wolf 2> /dev/null)" == "" ]]; then
   sudo docker build -t wolf ${basepath}/linux;
 fi
 
-sudo docker run -v ${basepath}:/wolf -it wolf;
+sudo docker run --rm -v ${basepath}:/wolf wolf;
 
 # docker created files are roots, well, it's hard to remove them then, you know
 find ${basepath} -user root -exec sudo chown -R $USER: {} +
