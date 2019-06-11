24b0c20932dda6029debfcc8d8962652cba24e66
Ondřej Lomič <ondrej.lomic@ysoft.com>
2019-06-11 14:14:37 +0200

    stats print whole number

--- src/plugins/stats.h
+++ src/plugins/stats.h
@@ -11,7 +11,7 @@ namespace wolf {
 class stats : public threaded_plugin {
  protected:
   void loop() override {
-    logger.info("Speed " + std::to_string(i / 1000) + "k events per minute.");
+    logger.info("Speed " + std::to_string(i) + " events per minute.");
     i = 0;
     std::this_thread::sleep_for(std::chrono::seconds(60));
   }
