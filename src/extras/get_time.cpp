0b29fd6bf4bee2a66202f57aeb3c2e42bcea1606
Ondřej Lomič <ondrej.lomic@ysoft.com>
2019-06-18 09:33:18 +0200

    fix date format

--- src/extras/get_time.cpp
+++ src/extras/get_time.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "get_time.h"
+
 std::string wolf::extras::get_date() {
   typedef std::chrono::system_clock Clock;
 
@@ -10,7 +11,12 @@ std::string wolf::extras::get_date() {
   std::time_t now_c = Clock::to_time_t(now);
   struct tm *parts = std::localtime(&now_c);
 
-  return std::to_string(1900 + parts->tm_year) + "." +
-      std::to_string(1 + parts->tm_mon) + "." +
-      std::to_string(parts->tm_mday);
+  auto year = std::to_string(1900 + parts->tm_year);
+  auto month = std::to_string(1 + parts->tm_mon);
+  auto day = std::to_string(parts->tm_mday);
+
+  if (month.size() == 1) month = "0" + month;
+  if (day.size() == 1) day = "0" + day;
+
+  return year + "." + month + "." + day;
 }
