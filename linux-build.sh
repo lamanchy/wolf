f5d2453fa46a7573c7d4c92a52ebf03ebc821158
Ondrej Lomic <ondrej.lomic@ysoft.com>
2019-09-16 13:06:35 +0200

    fix docker mount path

--- linux-build.sh
+++ linux-build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
+basepath="$(cd "$(dirname "$0")"; pwd -P)"
+
 if [ "$#" -eq 1 ]; then
-    basepath=$1
+    mount_path=$1
 else
-    basepath="$(cd "$(dirname "$0")"; pwd -P)"
+    mount_path=$basepath
 fi
 
 # if [[ "$(sudo docker images -q wolf 2> /dev/null)" == "" ]]; then
   sudo docker build -t wolf ${basepath}/linux;
 # fi
 
-sudo docker run --rm -v ${basepath}:/wolf wolf;
+sudo docker run --rm -v ${mount_path}:/wolf wolf;
 
 # docker created files are roots, well, it's hard to remove them then, you know
 find ${basepath} -user root -exec sudo chown -R $USER: {} +
