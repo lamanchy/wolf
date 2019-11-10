8e956f0ea9fbcb302d62bc23f41b3433f432dbc7
lomic <ondrej.lomic@ysoft.com>
2019-11-10 20:04:02 +0100

    one time libraries install

--- cmake/install_definitions.cmake
+++ cmake/install_definitions.cmake
@@ -1,5 +1,17 @@
 install(TARGETS wolf)
 
+install(CODE "
+if (NOT EXISTS ${CMAKE_INSTALL_PREFIX}/include)
+    message(STATUS \"installing libraries, only done for the first time\")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${WOLF_LIBS_PATH}/include ${CMAKE_INSTALL_PREFIX}/include)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${WOLF_LIBS_PATH}/LIB ${CMAKE_INSTALL_PREFIX}/LIB)
+else()
+    message(STATUS \"skipping libraries install, only done for the first time\")
+endif()
+")
+
 install(DIRECTORY ${CMAKE_SOURCE_DIR}/include
         DESTINATION .
-        FILES_MATCHING PATTERN "*.h")
+        MESSAGE_NEVER
+        FILES_MATCHING PATTERN "*.h"
+        )
