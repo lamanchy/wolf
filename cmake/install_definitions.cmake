dd5d360849ffc85349dc333cbb7785402d1cd8ed
lomic <ondrej.lomic@ysoft.com>
2019-11-10 15:43:14 +0100

    fix install destination

--- cmake/install_definitions.cmake
+++ cmake/install_definitions.cmake
@@ -1,6 +1,5 @@
 install(TARGETS wolf)
 
-install(
-        DIRECTORY ${CMAKE_SOURCE_DIR}/include
-        DESTINATION include
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/include
+        DESTINATION .
         FILES_MATCHING PATTERN "*.h")
