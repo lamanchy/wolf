cdd0922bccc9d99d1173e71a89ddaf747f200f60
lomic <ondrej.lomic@ysoft.com>
2019-11-11 10:43:35 +0100

    wolf as cmake add_subdirectory

--- cmake/build_libraries.cmake
+++ cmake/build_libraries.cmake
@@ -1,18 +1,18 @@
 # library path
-set(WOLF_LIBS_PATH ${CMAKE_BINARY_DIR}/libs)
+set(WOLF_LIBS_PATH ${CMAKE_CURRENT_BINARY_DIR}/libs)
 
 # build libraries in cofigure step
 # libraries are build just once, if build is successful, it is not repeated
 # clear cache to repeat build
 if (NOT WOLF_SKIP_LIBRARY_BUILD)
-    set(WOLF_LIBS_SOURCE ${CMAKE_BINARY_DIR}/lib_source)
+    set(WOLF_LIBS_SOURCE ${CMAKE_CURRENT_BINARY_DIR}/lib_source)
     file(MAKE_DIRECTORY ${WOLF_LIBS_SOURCE})
     execute_process(
             COMMAND ${CMAKE_COMMAND}
             -G ${CMAKE_GENERATOR}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DWOLF_LIBS_PATH=${WOLF_LIBS_PATH}
-            ${CMAKE_SOURCE_DIR}/libs
+            ${WOLF_PATH}/cmake/libs
 
             WORKING_DIRECTORY ${WOLF_LIBS_SOURCE}
             RESULT_VARIABLE RESULT
--- cmake/install_definitions.cmake
+++ cmake/install_definitions.cmake
@@ -4,13 +4,13 @@ install(CODE "
 if (NOT EXISTS ${CMAKE_INSTALL_PREFIX}/include)
     message(STATUS \"installing libraries, only done for the first time\")
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${WOLF_LIBS_PATH}/include ${CMAKE_INSTALL_PREFIX}/include)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${WOLF_LIBS_PATH}/LIB ${CMAKE_INSTALL_PREFIX}/LIB)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${WOLF_LIBS_PATH}/lib ${CMAKE_INSTALL_PREFIX}/lib)
 else()
     message(STATUS \"skipping libraries install, only done for the first time\")
 endif()
 ")
 
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/include
+install(DIRECTORY ${WOLF_PATH}/include
         DESTINATION .
         MESSAGE_NEVER
         FILES_MATCHING PATTERN "*.h"
