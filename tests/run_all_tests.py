a7ad9d13bef3af3c3a1b7d6970c6b0b3abfe85b5
lomic <ondrej.lomic@ysoft.com>
2020-02-12 10:34:43 +0100

    tests removed

--- tests/run_all_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from .end_to_end_tests.calf_parser_test import CalfParserTest
-from .end_to_end_tests.correlator_test import CorrelatorTest
-from .end_to_end_tests.parser_test import ParserTest
-
-if __name__ == '__main__':
-    unittest.main()
