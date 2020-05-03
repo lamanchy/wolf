eecab10fa59a0285cfcd62eb737dc80c1eadb045
lomic <ondrej.lomic@ysoft.com>
2020-05-03 19:41:44 +0200

    interval added to stats

--- include/plugins/stats.h
+++ include/plugins/stats.h
@@ -7,14 +7,15 @@ namespace wolf {
 
 class stats : public threaded_plugin {
  public:
-  explicit stats(std::string description) : description(std::move(description)) {}
+  explicit stats(std::string description, unsigned interval = 60) :
+      description(std::move(description)), interval(interval) {}
   stats() : stats("stats") {}
 
  protected:
   void loop() override {
-    logger.info(description + " speed " + std::to_string(i) + " events per min.");
+    logger.info(description + " speed " + std::to_string(int(i * 60 / interval)) + " events per min.");
     i = 0;
-    get_loop_sleeper().sleep_for(std::chrono::seconds(60));
+    get_loop_sleeper().sleep_for(std::chrono::seconds(interval));
   }
 
   void process(json &&message) override {
@@ -25,6 +26,7 @@ class stats : public threaded_plugin {
  private:
   std::atomic<unsigned long> i{0};
   std::string description;
+  unsigned interval = 60;
 };
 
 }
